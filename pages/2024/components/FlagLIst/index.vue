92b8212ffc772e15da6ba8b161de3a8ecbb3e5c6
LQYld <54627537+LQYld@users.noreply.github.com>
2024-01-06 19:29:21 +0800

    feat: Added 2024 Flags

---
+++ pages/2024/components/FlagLIst/index.vue
@@ -0,0 +1,202 @@
+<template>
+  <div v-if="data">
+    <h3 class="slide-enter-content">{{ data?.title }}</h3>
+    <div>
+      <svg viewBox="0 0 0 0" class="absolute opacity-0 z-0">
+        <defs>
+          <linearGradient id="boxGradient" gradientUnits="userSpaceOnUse" x1="0" y1="0" x2="25" y2="25">
+            <stop offset="0%" :stop-color="stopColor" />
+            <stop offset="100%" :stop-color="stopColor" />
+          </linearGradient>
+
+          <linearGradient id="lineGradient">
+            <stop offset="0%" :stop-color="stopColor" />
+            <stop offset="100%" :stop-color="stopColor" />
+          </linearGradient>
+
+          <path id="todo__line" stroke="url(#lineGradient)" d="M21 12.3h168v0.1z"></path>
+          <path id="todo__box" stroke="url(#boxGradient)"
+            d="M21 12.7v5c0 1.3-1 2.3-2.3 2.3H8.3C7 20 6 19 6 17.7V7.3C6 6 7 5 8.3 5h10.4C20 5 21 6 21 7.3v5.4"></path>
+          <path id="todo__check" stroke="url(#boxGradient)" d="M10 13l2 2 5-5"></path>
+          <circle id="todo__circle" cx="13.5" cy="12.5" r="10"></circle>
+        </defs>
+      </svg>
+      <div class="todo-list">
+        <label class="todo" v-for="(node, nodeIndex) in data?.flagList" :key="`todo_component_${nodeIndex}`">
+          <input class="todo-state" type="checkbox" :checked="node.checked" />
+
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 25"
+            class="todo-icon">
+            <use xlink:href="#todo__line" class="todo-line"></use>
+            <use xlink:href="#todo__box" class="todo-box"></use>
+            <use xlink:href="#todo__check" class="todo-check"></use>
+            <use xlink:href="#todo__circle" class="todo-circle"></use>
+          </svg>
+
+          <p class="todo-text">{{ node.value }}</p>
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  data: {
+    title: String,
+    flagList: Array<{
+      checked: false,
+      value: string,
+    }>
+  },
+})
+
+const stopColor = 'rgba(133, 170, 185, 1)'
+</script>
+
+<style scoped>
+
+h3 {
+  font-size: 1.7em;
+  font-weight: 400;
+  line-height: 1.4;
+}
+
+.todo-list {
+  /* padding: 0 10px; */
+  margin: auto;
+}
+
+.todo {
+  position: relative;
+  display: block;
+  padding: 10px 10px 10px 16%;
+  margin: 0 auto;
+  cursor: pointer;
+  border-bottom: solid 1px #ddd;
+}
+
+.todo:last-child {
+  border-bottom: none;
+}
+
+.todo-state {
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+}
+
+.todo-text {
+  max-width: 28em;
+  font-size: 1.6em;
+  font-weight: 300;
+  line-height: 1.4;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  transition: all var(--duration)/2 linear var(--duration)/2;
+}
+
+.todo-icon {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  height: auto;
+  fill: none;
+  stroke: rgba(133, 170, 185, 1);
+  stroke-width: 2;
+  stroke-linejoin: round;
+  stroke-linecap: round;
+}
+
+
+.todo-line,
+.todo-box,
+.todo-check {
+  transition: stroke-dashoffset var(--duration) cubic-bezier(.9, .0, .5, 1);
+}
+
+
+.todo-circle {
+  stroke: rgba(133, 170, 185, 1);
+  stroke-dasharray: 1 6;
+  stroke-width: 0;
+  transform-origin: 13.5px 12.5px;
+  transform: scale(0.4) rotate(0deg);
+  animation: none var(--duration) linear;
+  /* //cubic-bezier(.08,.56,.04,.98); */
+
+}
+
+@keyframes explode {
+
+  /* //0% { stroke-width: 0; transform: scale(0.5) rotate(0deg); } */
+  30% {
+    stroke-width: 3;
+    stroke-opacity: 1;
+    transform: scale(0.8) rotate(40deg);
+
+    /* //animation-timing-function: cubic-bezier(.89,.01,.95,.51); */
+  }
+
+  100% {
+    stroke-width: 0;
+    stroke-opacity: 0;
+    transform: scale(1.1) rotate(60deg);
+
+    /* //animation-timing-function: cubic-bezier(.08,.56,.04,.98); */
+  }
+}
+
+.todo-box {
+  stroke-dasharray: 56.1053, 56.1053;
+  stroke-dashoffset: 0;
+  transition-delay: var(--duration) * 0.2;
+}
+
+.todo-check {
+  stroke: rgba(133, 170, 185, 1);
+  stroke-dasharray: 9.8995, 9.8995;
+  stroke-dashoffset: 9.8995;
+  transition-duration: var(--duration) * 0.4;
+}
+
+.todo-line {
+  stroke-dasharray: 168, 1684;
+  stroke-dashoffset: 168;
+}
+
+.todo-circle {
+  animation-delay: var(--duration) * 0.7;
+  animation-duration: var(--duration) * 0.7;
+}
+
+
+.todo-state:checked~.todo-text {
+  transition-delay: 0s;
+  color: rgba(133, 170, 185, 1);
+  opacity: 0.6;
+}
+
+.todo-state:checked~.todo-icon .todo-box {
+  stroke-dashoffset: 56.1053;
+  transition-delay: 0s;
+}
+
+.todo-state:checked~.todo-icon .todo-line {
+  stroke-dashoffset: -8;
+}
+
+.todo-state:checked~.todo-icon .todo-check {
+  stroke-dashoffset: 0;
+  transition-delay: var(--duration) * 0.6;
+}
+
+.todo-state:checked~.todo-icon .todo-circle {
+  animation-name: explode;
+}
+</style>
--- pages/2024/index.vue
+++ pages/2024/index.vue
@@ -2,22 +2,115 @@
   <div>
     <div class="flex w-full justify-between">
       <div class="relative pt-20">
-        <span>2024</span>
-        <h1 class="font-medium text-4xl">Hey, I am Qingyun119.</h1>
-        <p>Goodbye, 2023! You have brought us countless memories and experiences, making us grow and become strong. Thank
+        <span class="slide-enter-content">2024</span>
+        <h1 class="font-medium text-4xl slide-enter-content">Hey, I am Qingyun119.</h1>
+        <p class="slide-enter-content"><a>Goodbye, 2023!</a> You have brought us countless memories and experiences,
+          making us
+          grow and become strong. Thank
           you for your company, and we will welcome the new year with hope and dreams.</p>
-        <p>2024 is coming with new hopes and dreams. Let's leave behind the troubles and troubles of the past, greet the
+        <p class="slide-enter-content"><a>2024</a> is coming with new hopes and dreams. Let's leave behind the troubles
+          and
+          troubles of the past, greet the
           new year with a smile and courage, and believe that the future will be better!</p>
-        <p>This is my OKR (Objectives and Key Results) planning for 2024, aiming to achieve personal growth and
+        <p class="slide-enter-content">This is my <a>OKR (Objectives and Key Results)</a> planning for <a>2024</a>, aiming
+          to achieve
+          personal growth and
           development.</p>
+        <FlagList :data="workFlag" />
+        <FlagList :data="lifeFlag" />
+        <FlagList :data="personalFlag" />
+        <h3 class="slide-enter-content">End</h3>
       </div>
     </div>
+    <img class="md:fixed bottom-0 right-0 m-auto w-2/6 animate__animated animate__fadeInUp"
+      src="~/assets/img/flag-theme.png" />
   </div>
 </template>
 
 <script setup lang="ts">
-// const goodbyeDocs = '再见了，2023！你给我们带来了无数的回忆和经历，让我们成长，让我们坚强。感谢你的陪伴，我们将怀着希望和梦想迎接新的一年。'
-// const helloDocs = '2024 年，带着新的希望和梦想，正向我们走来。让我们抛开过去的烦恼和困扰，用微笑和勇气迎接新的一年，相信未来一定会更加美好！'
+import FlagList from './components/FlagLIst/index.vue';
+
+const workFlag = {
+  title: "💼 Work",
+  flagList: [
+    {
+      value: "Participate in the code review of colleagues' pull requests as much as possible."
+    },
+    {
+      value: "Analyze the project design and considerations of the company's internal community."
+    },
+    {
+      value: "Spend over 50% of the time coding, over 20% of the time participating in business."
+    },
+    {
+      value: "Write documents in a timely manner to summarize and review every requirement."
+    },
+    {
+      value: 'At least promote one rank.'
+    }
+  ]
+}
+
+const lifeFlag = {
+  title: "🏖️ Life",
+  flagList: [
+    {
+      value: "Go back to your hometown several times and spend more time with your parents."
+    },
+    {
+      value: "Take a spontaneous trip in China and gain an in-depth understanding of the local customs and culture."
+    },
+    {
+      value: "Discard the previous lifestyle and pursue a comfortable and free attitude towards life."
+    },
+    {
+      value: "Organize several camping activities to relax the body and mind and get close to nature."
+    }
+  ]
+}
+
+const personalFlag = {
+  title: "👨🏻‍💻 Personal",
+  flagList: [
+    {
+      value: "With health as the top priority, exercise to lose weight and restore previous weight."
+    },
+    {
+      value: "Publish more than 50 articles externally, with no restrictions on topics, in order to acquire a small number of subscribers."
+    },
+    {
+      value: "Continue to delve into front-end technology and become proficient in nodejs, java, sql, nginx, and docker."
+    },
+    {
+      value: "Read 24 books with no limitations on the scope, and summarize the insights and experiences."
+    },
+    {
+      value: "Summarize the front-end resume design tutorial and summarize the negotiation话术of hard and soft skills in the interview."
+    },
+    {
+      value: "Improve the ability to write true and false codes, and reflect the data structure design and algorithmic capabilities in programming."
+    },
+    {
+      value: "On the LeetCode platform, strive to complete all algorithmic problems of easy and medium difficulty."
+    },
+    {
+      value: "Strive to submit code on GitHub every day and turn all contribution values in 2024 green."
+    },
+    {
+      value: "Finish learning the source code of Vue and React, and submit more than 10 technical PRs in the open source community, with no restrictions on projects.",
+    },
+    {
+      value: "Try publishing works on media platforms, including but not limited to Douyin, Twitter, Xiaohongshu, YouTube, and Bilibili."
+    },
+    {
+      value: "Attempt to develop a side business and earn the first pot of gold. There are no restrictions on the direction or industry."
+    },
+    {
+      value: "Improve English proficiency to the level where I can directly read technical documents without frequently using translation tools."
+    }
+  ]
+}
+
 </script>
 
 <style scoped>
@@ -29,6 +122,7 @@ span {
   color: transparent;
   position: absolute;
   top: 0;
+  user-select: none;
 }
 
 p {
@@ -42,8 +136,66 @@ p {
   margin-inline-end: 0px;
 }
 
+a {
+  text-decoration: none;
+  white-space: nowrap;
+  color: #121314;
+  position: relative;
+  cursor: pointer;
+}
+
+a:after {
+  content: '';
+  position: absolute;
+  z-index: -1;
+  top: 66%;
+  left: -0.1em;
+  right: -0.1em;
+  bottom: 0;
+  transition: top 200ms cubic-bezier(0, 0.8, 0.13, 1);
+  background-color: var(--primary);
+}
+
+a:hover:after {
+  top: 0%;
+}
+
+.animate__animated {
+  --animate-duration: 1s;
+  --animate-delay: 1s;
+  --animate-repeat: 1;
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+  -webkit-animation-duration: var(--animate-duration);
+  animation-duration: var(--animate-duration);
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}
+
+@keyframes fadeInUp {
+  from {
+    opacity: 0;
+    -webkit-transform: translate3d(0, 100%, 0);
+    transform: translate3d(0, 100%, 0);
+  }
+
+  to {
+    opacity: 1;
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+}
+
+.animate__fadeInUp {
+  -webkit-animation-name: fadeInUp;
+  animation-name: fadeInUp;
+}
+
+.pb-33vw {
+  padding-bottom: 33.33vw;
+}
+
 h3 {
-  max-width: 28em;
   font-size: 1.7em;
   font-weight: 400;
   line-height: 1.4;
