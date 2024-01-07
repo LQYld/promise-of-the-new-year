9fff3ab74f91868be9cd5cd40fb79390be2e78d9
LQYld <54627537+LQYld@users.noreply.github.com>
2024-01-07 16:57:00 +0800

    fix: Fixed word bug

--- pages/2024/index.vue
+++ pages/2024/index.vue
@@ -34,7 +34,7 @@
 </template>
 
 <script setup lang="ts">
-import FlagList from './components/FlagLIst/index.vue';
+import FlagList from './components/FlagList/index.vue';
 
 const workFlag = {
   title: "💼 Work",
